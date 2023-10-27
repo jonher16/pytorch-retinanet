8e56420f4c6d527812e2d46ef64f72af7d4dd6ed
jonher16 <1999.jon@gmail.com>
2023-10-27 18:42:26 +0900

    changed inference

--- inference.sh
+++ inference.sh
@@ -2,6 +2,6 @@
 
 python visualize.py \
     --dataset csv \
-    --csv_classes /home/jonher16/Documents/pytorch-retinanet/data/with_dist/classes.csv \
-    --csv_val /home/jonher16/Documents/pytorch-retinanet/data/with_dist/test.csv \
-    --model /home/jonher16/Documents/pytorch-retinanet/models/csv_retinanet_39.pt
+    --csv_classes /home/jonher16/Documents/retina-pytorch/data/with_dist/classes.csv \
+    --csv_val /home/jonher16/Documents/retina-pytorch/data/with_dist/test.csv \
+    --model /home/jonher16/Documents/retina-pytorch/models/csv_retinanet_99.pt
